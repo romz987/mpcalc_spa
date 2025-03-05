d3255291be89d88aaab2eea8925803735f7a71c3
romz987pc <eesistance@gmail.com>
2025-03-05 12:12:09 +0300

    feat: base structure

--- mpcalcspa/src/App.jsx
+++ mpcalcspa/src/App.jsx
@@ -1,6 +1,16 @@
+import './assets/styles/reset.css'
+import './assets/styles/global.css'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import MainContent from './components/layout/MainContent'
+
+
 function App() {
-  return <h1>Hello, user!</h1>;
+  return <div className="container">
+          <Header />
+          <MainContent />
+          <Footer />
+         </div>
 }
 
 export default App;
-
---
+++ mpcalcspa/src/components/layout/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <section className="content">
+      <div className="navigation"></div>
+      <div className="calc-window"></div> 
+    </section>
+  );
+}
+
+export default MainContent
